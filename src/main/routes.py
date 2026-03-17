c9d2eaf27fad20f5d345d5a2bb812a1a2c79963b
Ananya Jain <ananya.23fe10cse00086@muj.manipal.edu>
2026-03-18 00:13:38 +0530

    comments

--- src/main/routes.py
+++ src/main/routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request, jsonify
 from models import TestResult
 from extensions import db
-
+#api endpoints and dashboard route
 main = Blueprint('main', __name__)
 
 @main.route("/")
